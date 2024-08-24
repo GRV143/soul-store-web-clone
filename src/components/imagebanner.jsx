9367894da100774c000d87a1d4b728823e8d3cab
grv143 <gauravvarun13517@gmail.com>
2024-08-25 01:19:50 +0530

    m'images added in image banner'

--- src/components/imagebanner.jsx
+++ src/components/imagebanner.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
-
-function Imagebanner() {
+function imagebanner() {
   return (
-    <>
-    
-    <div>
-        <img src="https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/banner-images/homepage_banner_copy_8.jpg?format=webp&w=1366&dpr=1.0" alt="no image found" />
-        </div>
-   
-  </>
+    <div className='flex'>
+         <div className='flex-shrink-0 ' >
+          <img  src="https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/banner-images/homepage_banner_copy_8.jpg?format=webp&w=1366&dpr=1.0" alt="no image found" />
+          </div  >
+          <div className='flex-shrink-0 '>
+          <img src="https://prod-img.thesouledstore.com/public/theSoul/storage/mobile-cms-media-prod/banner-images/OVERSIZED_TSHIRT-_HOMEPAGE.jpg?format=webp&w=1366&dpr=1.0" alt="" />
+          </div >
+          <div className='flex-shrink-0 '>
+          <img className='w-full h-full' src="https://prod-img.thesouledstore.com/public/theSoul/uploads/themes/8205220240812143714.jpg?format=webp&w=1366&dpr=1.0" alt="" />
+          </div>
+    </div>
+
   )
 }
 
-export default Imagebanner
+export default imagebanner
